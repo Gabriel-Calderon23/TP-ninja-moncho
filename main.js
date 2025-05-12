7a6d8e2c306f70e51bc7dce274f4cef7689b45cd
Gabriel calderón <165054985+Gabriel-Calderon23@users.noreply.github.com>
2025-05-12 04:36:32 -0300

    Revert "Feature/ninja moncho p12"

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import Menu from "./scenes/Menu.js";
-import Game from "./scenes/Game.js";
-import Finish from "./scenes/finish.js";
+import HelloWorldScene from "./scenes/HelloWorldScene.js";
 
 // Create a new Phaser config object
 const config = {
@@ -29,7 +27,7 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [Menu,Game,Finish,],
+  scene: [HelloWorldScene],
 };
 
 // Create a new Phaser game instance
