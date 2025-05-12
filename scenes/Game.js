99b7c7498c6241d7b57a1ff3e251c55fc8265e10
Gabriel-Calderon23 <gc002905@>
2025-05-11 21:12:08 -0300

    cargue el personaje,la plataforma y les agregue fisicas

--- scenes/Game.js
+++ scenes/Game.js
@@ -21,12 +21,26 @@ export default class Game extends Phaser.Scene {
     this.load.image("diamod", "./public/assets/diamond.png");
     this.load.image("player", "./public/assets/ninja.png");
     this.load.image("square", "./public/assets/square.png")
-    this.load.image("triangle", "./public/assets/triangle.png")
+    this.load.image("triangle", "./public/assets/triangle.png");
     
   }
 
   create() {
     // create game objects
+    
+    this.add.image(400, 300, "sky").setScale(1.85) // carga y muestra la imagen 
+
+   this.platforms = this.physics.add.staticGroup(); // le agrego fisicas a la plataforma
+  
+   this.platforms.create(400, 568, "ground").setScale(2).refreshBody(); // carga la plataforma y la escala x2
+   this.player = this.physics.add.sprite(400, 450, "player").setScale(0.1);// carga el personaje y le reduce el tamaño
+   //this.player.setBounce(0.2);
+
+   this.player.setCollideWorldBounds(true); // hace que el personaje golpe con el borde del mundo (pantalla)
+   this.physics.add.collider(this.player, this.platforms);// hace que la plataforma y el personaje colicionen
+
+
+
     
   }
 
